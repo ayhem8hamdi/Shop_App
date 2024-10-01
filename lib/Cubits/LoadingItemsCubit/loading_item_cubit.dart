8cff48469fc031604eb472ba315523183f10a139
ayhem8hamdi <ayhemhamdi24@gmail.com>
2024-10-01 23:34:27 +0100

    Create getAllProducts From FireBase method & Create LoadingItemCubit Function

--- lib/Cubits/LoadingItemsCubit/loading_item_cubit.dart
+++ lib/Cubits/LoadingItemsCubit/loading_item_cubit.dart
@@ -1,6 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopp_app/Cubits/LoadingItemsCubit/loading_items_cubit_states.dart';
+import 'package:shopp_app/Helper/firebase_operation.dart';
+import 'package:shopp_app/Models/category.dart';
 
 class LoadingItemCubit extends Cubit<LoadingItemsCubitStates> {
   LoadingItemCubit() : super(ItemInitial());
+
+  void fetchData() async {
+    try {
+      emit(ItemLoading());
+
+      List<Category> categories =
+          await getProducts(collectionName: 'ProductsItems');
+
+      emit(ItemLoaded());
+    } catch (e) {
+      emit(ItemError());
+    }
+  }
 }
--- lib/Models/category.dart
+++ lib/Models/category.dart
@@ -1,11 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Category {
   final String title;
+  final String id;
   final String image;
   int? stock;
   final double price;
   Category(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.image,
       this.stock = 120,
       required this.price});
+  factory Category.fetchFireBaseData(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Category(
+        id: doc.id,
+        image: data['image'],
+        price: data['price'],
+        title: data['title'],
+        stock: data['stock']);
+  }
 }
