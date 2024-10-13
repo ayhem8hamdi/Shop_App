1d5a7862804920182e6984d0456e9a3ac1090fe8
ayhem8hamdi <ayhemhamdi24@gmail.com>
2024-10-13 22:52:16 +0100

    Handle quantity & out of stock exception

--- lib/Widgets/item_screen_icon.dart
+++ lib/Widgets/item_screen_icon.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
 
 class ItemScreenIcon extends StatelessWidget {
-  const ItemScreenIcon({super.key, this.onTap, required this.icon});
-  final void Function()? onTap;
+  const ItemScreenIcon({
+    super.key,
+    required this.icon,
+    this.onTap,
+    this.isEnabled = true,
+  });
+
   final IconData icon;
+  final VoidCallback? onTap;
+  final bool isEnabled;
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: onTap,
-        icon: Container(
+    return GestureDetector(
+      onTap: isEnabled ? onTap : null,
+      child: Opacity(
+        opacity: isEnabled ? 1.0 : 0.4,
+        child: Container(
           padding: const EdgeInsets.all(4),
           decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(45),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 8)
-              ]),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(45),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 8,
+              ),
+            ],
+          ),
           child: Icon(
-            size: 22,
             icon,
+            size: 22,
             color: Colors.black87.withOpacity(0.78),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/Widgets/quantity_counter_widget.dart
+++ lib/Widgets/quantity_counter_widget.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopp_app/Cubits/AddToCardCubit/add_to_card_cubit.dart';
+import 'package:shopp_app/Cubits/AddToCardCubit/add_to_card_states.dart';
 import 'package:shopp_app/Cubits/UpdateItemsCubit/update_item_cubit.dart';
-import 'package:shopp_app/Cubits/UpdateItemsCubit/update_item_cubit_states.dart';
 import 'package:shopp_app/Methods/scaffold_messenger.dart';
+import 'package:shopp_app/Models/card.dart';
 import 'package:shopp_app/Models/category.dart';
 import 'package:shopp_app/Widgets/item_screen_icon.dart';
 
 class QuantityCounterWidget extends StatefulWidget {
-  const QuantityCounterWidget(
-      {super.key, required this.color, required this.cat});
+  const QuantityCounterWidget({
+    super.key,
+    required this.color,
+    required this.cat,
+  });
 
   final Color color;
   final Category cat;
+
   @override
   State<QuantityCounterWidget> createState() => _QuantityCounterWidgetState();
 }
@@ -26,53 +32,68 @@ class _QuantityCounterWidgetState extends State<QuantityCounterWidget> {
       });
       BlocProvider.of<UpdateItemCubit>(context).counter = counter;
     } else {
-      scafMess(context, 'U \'ve reached max Quantity');
+      scafMess(context, 'You\'ve reached the max quantity');
     }
   }
 
   void minus() {
-    setState(() {
-      if (counter > 0) {
+    if (counter > 0) {
+      setState(() {
         counter--;
-      }
-    });
-    BlocProvider.of<UpdateItemCubit>(context).counter = counter;
+      });
+      BlocProvider.of<UpdateItemCubit>(context).counter = counter;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ItemScreenIcon(
-          icon: Icons.remove,
-          onTap: minus,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: BlocListener<UpdateItemCubit, UpdateItemCubitStates>(
-            listener: (context, state) {
-              if (state is ItemUpdated) {
-                setState(() {
-                  counter = 0;
-                });
-                BlocProvider.of<UpdateItemCubit>(context).counter = 0;
-              }
-            },
-            child: Text(
-              counter < 10 && counter != 0 ? '0$counter' : '$counter',
-              style: TextStyle(
-                fontSize: 19,
-                color: widget.color,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+    List<Cart> cart = BlocProvider.of<AddToCardCubit>(context).selectedProducts;
+
+    final existingProductIndex =
+        cart.indexWhere((product) => product.title == widget.cat.title);
+
+    int existingProductQte =
+        existingProductIndex != -1 ? cart[existingProductIndex].qte : 0;
+
+    return BlocListener<AddToCardCubit, AddToCardStates>(
+      listener: (context, state) {
+        if (state is AddtoCardSucces) {
+          setState(() {
+            counter = 0;
+          });
+          BlocProvider.of<UpdateItemCubit>(context).counter = 0;
+        }
+      },
+      child: Row(
+        children: [
+          ItemScreenIcon(
+            icon: Icons.remove,
+            onTap: counter > 0 ? minus : null,
+            isEnabled: counter > 0,
           ),
-        ),
-        ItemScreenIcon(
-          icon: Icons.add,
-          onTap: add,
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: buildCounterText(),
+          ),
+          ItemScreenIcon(
+            icon: Icons.add,
+            onTap:
+                (existingProductQte + counter < widget.cat.stock) ? add : null,
+            isEnabled: existingProductQte + counter < widget.cat.stock,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildCounterText() {
+    return Text(
+      counter < 10 && counter != 0 ? '0$counter' : '$counter',
+      style: TextStyle(
+        fontSize: 19,
+        color: widget.color,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 }
