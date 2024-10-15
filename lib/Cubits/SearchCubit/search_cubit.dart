5c73744d54c95b12669077f7b611ec01e2065b31
ayhem8hamdi <ayhemhamdi24@gmail.com>
2024-10-15 16:44:41 +0100

    Create Cubit Function

--- lib/Cubits/SearchCubit/search_cubit.dart
+++ lib/Cubits/SearchCubit/search_cubit.dart
@@ -1,6 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopp_app/Cubits/SearchCubit/search_cubit_states.dart';
+import 'package:shopp_app/Models/category.dart';
 
 class SearchCubit extends Cubit<SearchListStates> {
   SearchCubit() : super(SarchListInitial());
+
+  void filter(List<Category> categories, String searchTerm) {
+    if (searchTerm.isEmpty) {
+      emit(SarchListInitial());
+      return;
+    }
+
+    final filteredTitles = categories
+        .where((category) {
+          return category.title
+              .toLowerCase()
+              .contains(searchTerm.toLowerCase());
+        })
+        .map((category) => category.title)
+        .toList();
+
+    emit(SarchListFiltred(filteredTitles));
+  }
 }
--- lib/Cubits/SearchCubit/search_cubit_states.dart
+++ lib/Cubits/SearchCubit/search_cubit_states.dart
@@ -2,4 +2,7 @@ class SearchListStates {}
 
 class SarchListInitial extends SearchListStates {}
 
-class SarchListFiltred extends SearchListStates {}
+class SarchListFiltred extends SearchListStates {
+  final List<String> filtredNames;
+  SarchListFiltred(this.filtredNames);
+}
