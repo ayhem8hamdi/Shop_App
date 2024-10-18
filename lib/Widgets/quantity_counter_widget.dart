73feffc7582eb46abd317af329484a9830d3feeb
ayhem8hamdi <ayhemhamdi24@gmail.com>
2024-10-18 23:45:51 +0100

    Create Add to Cart Check

--- lib/Widgets/quantity_counter_widget.dart
+++ lib/Widgets/quantity_counter_widget.dart
@@ -7,6 +7,7 @@ import 'package:shopp_app/Cubits/UpdateItemsCubit/update_item_cubit.dart';
 import 'package:shopp_app/Methods/scaffold_messenger.dart';
 import 'package:shopp_app/Models/category.dart';
 import 'package:shopp_app/Widgets/item_screen_icon.dart';
+import 'package:shopp_app/Models/card.dart';
 
 class QuantityCounterWidget extends StatefulWidget {
   const QuantityCounterWidget(
@@ -22,14 +23,25 @@ class QuantityCounterWidget extends StatefulWidget {
 class _QuantityCounterWidgetState extends State<QuantityCounterWidget> {
   int counter = 0;
 
+  int _getQuantityInCart() {
+    final cubit = BlocProvider.of<AddToCardCubit>(context);
+    final existingProduct = cubit.selectedProducts.firstWhere(
+      (product) => product.title == widget.cat.title,
+      orElse: () => Cart(image: '', title: '', qte: 0, price: 0),
+    );
+    return existingProduct.qte;
+  }
+
   void add() {
-    if (counter < widget.cat.stock) {
+    int totalQuantityInCart = _getQuantityInCart() + counter;
+
+    if (totalQuantityInCart < widget.cat.stock) {
       setState(() {
         counter++;
       });
       BlocProvider.of<UpdateItemCubit>(context).counter = counter;
     } else {
-      scafMess(context, 'You\'ve reached the max quantity');
+      scafMess(context, 'You\'ve reached the max quantity in stock');
     }
   }
 
@@ -66,12 +78,17 @@ class _QuantityCounterWidgetState extends State<QuantityCounterWidget> {
           ),
           ItemScreenIcon(
             icon: Icons.add,
-            onTap: counter < widget.cat.stock ? add : null,
-            isEnabled: counter < widget.cat.stock,
+            onTap: _canAddMore() ? add : null,
+            isEnabled: _canAddMore(),
           ),
         ],
       ),
     );
+  }
+
+  bool _canAddMore() {
+    int totalQuantityInCart = _getQuantityInCart() + counter;
+    return totalQuantityInCart < widget.cat.stock;
   }
 
   Widget _buildCounterText() {
